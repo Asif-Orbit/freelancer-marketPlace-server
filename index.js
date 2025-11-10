55007107985239e5554f05fb65f533b70827e726
Asif-Orbit <asifsweuestc@gmail.com>
2025-11-10 14:04:54 +0800

    install express , node , cors, dotenv and connect mogodb using env

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// mongodb
+
+  const uri = process.env.MONGO_URI;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Smart Deal Server is Running!");
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const db = client.db("smart_db");
+    const productsCollection = db.collection("products");
+
+    app.post("/allJobs", async (req, res) => {
+      const newJob = req.body;
+      const result = await productsCollection.insertOne(newJob);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`smart deal server is running on port : ${port}`);
+});
