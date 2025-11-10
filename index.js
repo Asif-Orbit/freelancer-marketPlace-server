60005add40a92e584332f6d9bf5b4e196f6711bb
Asif-Orbit <asifsweuestc@gmail.com>
2025-11-10 20:55:55 +0800

    implement job details page single get data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -22,12 +23,12 @@ const client = new MongoClient(uri, {
 });
 
 app.get("/", (req, res) => {
-  res.send("Smart Deal Server is Running!");
+  res.send("Freelance MarketPlace Server is Running!");
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server
     await client.connect();
     const db = client.db("freelancerDB");
     const productsCollection = db.collection("Jobs");
@@ -51,6 +52,17 @@ async function run() {
       const result = await productsCollection.insertOne(jobWithDate);
       res.send(result);
     });
+    app.get("/allJobs/jobDetails/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        console.log(id)
+        const job = await productsCollection.findOne({ _id: new ObjectId(id) });
+        if (!job) return res.status(404).send({ message: "Job not found" });
+        res.status(200).send(job);
+      } catch (error) {
+        res.status(500).send({ message: "Failed to fetch job details" });
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -63,5 +75,5 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`smart deal server is running on port : ${port}`);
+  console.log(`Freelance MarketPlace server is running on port : ${port}`);
 });
